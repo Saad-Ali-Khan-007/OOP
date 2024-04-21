4ef21a788dfdfd376c96b69008d1cc43f4790ddb
Saad Ali Khan <saadalikhan042@gmail.com>
2024-04-21 21:42:14 +0500

    working on pattern

--- lab2/q11.cpp
+++ lab2/q11.cpp
@@ -10,7 +10,72 @@ int startlab2()
 
 int l2q11()
 {
+    // Pattern 1
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 5; j > i; j--)
+        {
+            cout << "  ";
+        }
+        for (int k = 0; k < i; k++)
+        {
+            cout << "* ";
+        }
+        for (int l = 0; l < i + 1; l++)
+        {
 
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << "  ";
+        }
+        for (int k = 4; k > i; k--)
+        {
+            cout << "* ";
+        }
+        for (int l = 5; l > i; l--)
+        {
+
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    // Pattern 2
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (i == 0 || i == 4 ||
+                j == 0 || j == 4)
+            {
+                cout << "* ";
+            }
+            else
+            {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+    // Pattern 3
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 5; j > i; j--)
+        {
+            cout << "* ";
+        }
+        for (int k = 1; k < i; k++)
+        {
+            cout << k << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
 
