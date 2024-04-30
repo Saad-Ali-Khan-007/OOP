ff1137d1ae455605c9ae3c56170bb5a5c421f05d
Saad Ali Khan <saadalikhan042@gmail.com>
2024-04-30 21:36:25 +0500

    q11 patterns completed

--- lab2/q11.cpp
+++ lab2/q11.cpp
@@ -10,72 +10,143 @@ int startlab2()
 
 int l2q11()
 {
-    // Pattern 1
-    for (int i = 0; i < 4; i++)
+    // // Pattern 1
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     for (int j = 5; j > i; j--)
+    //     {
+    //         cout << "  ";
+    //     }
+    //     for (int k = 0; k < i; k++)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     for (int l = 0; l < i + 1; l++)
+    //     {
+
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     for (int j = 0; j < i + 1; j++)
+    //     {
+    //         cout << "  ";
+    //     }
+    //     for (int k = 4; k > i; k--)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     for (int l = 5; l > i; l--)
+    //     {
+
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+    // cout << endl;
+    // cout << endl;
+    // // Pattern 2
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     for (int j = 0; j < 5; j++)
+    //     {
+    //         if (i == 0 || i == 4 ||
+    //             j == 0 || j == 4)
+    //         {
+    //             cout << "* ";
+    //         }
+    //         else
+    //         {
+    //             cout << "  ";
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+    // cout << endl;
+    // cout << endl;
+
+    // // Pattern 3
+    // for (int i = 1; i < 6; i++)
+    // {
+    //     for (int j = 5; j > i; j--)
+    //     {
+    //         cout << "  ";
+    //     }
+    //     for (int k = i; k >= 1; k--)
+    //     {
+
+    //         cout << k << " ";
+    //     }
+    //     for (int l = 2; l <= i; l++)
+    //     {
+    //         cout << l << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // cout << endl;
+    // cout << endl;
+
+    // Pattern 4
+
+    // for (int i = 0; i <= 5; i++)
+    // {
+    //     for (int j = 0; j < i + 1; j++)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+    // cout << endl;
+    // cout << endl;
+    // cout << endl;
+
+    // Pattern 5
+
+    for (int i = 0; i <= 6; i++)
     {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << "* ";
+        }
         for (int j = 5; j > i; j--)
         {
             cout << "  ";
         }
-        for (int k = 0; k < i; k++)
+        for (int j = 5; j > i; j--)
         {
-            cout << "* ";
+            cout << "  ";
         }
-        for (int l = 0; l < i + 1; l++)
+        for (int j = 0; j < i + 1; j++)
         {
-
             cout << "* ";
         }
         cout << endl;
     }
-    for (int i = 0; i < 5; i++)
+
+    for (int i = 0; i <= 5; i++)
     {
+        for (int j = 5; j > i; j--)
+        {
+            cout << "* ";
+        }
         for (int j = 0; j < i + 1; j++)
         {
             cout << "  ";
         }
-        for (int k = 4; k > i; k--)
+        for (int j = 0; j < i + 1; j++)
         {
-            cout << "* ";
+            cout << "  ";
         }
-        for (int l = 5; l > i; l--)
-        {
-
-            cout << "* ";
-        }
-        cout << endl;
-    }
-
-    // Pattern 2
-    for (int i = 0; i < 5; i++)
-    {
-        for (int j = 0; j < 5; j++)
-        {
-            if (i == 0 || i == 4 ||
-                j == 0 || j == 4)
-            {
-                cout << "* ";
-            }
-            else
-            {
-                cout << "  ";
-            }
-        }
-        cout << endl;
-    }
-    // Pattern 3
-    for (int i = 0; i < 5; i++)
-    {
         for (int j = 5; j > i; j--)
         {
             cout << "* ";
         }
-        for (int k = 1; k < i; k++)
-        {
-            cout << k << " ";
-        }
         cout << endl;
     }
+
     return 0;
 }
 
