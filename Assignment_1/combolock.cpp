63d1ef4f64e04a8bf5c04e2e03abc5a8e2baf126
Saad Ali Khan <saadalikhan042@gmail.com>
2024-06-08 13:09:15 +0500

    error handling remaining

--- Assignment_1/combolock.cpp
+++ Assignment_1/combolock.cpp
@@ -4,17 +4,11 @@ using namespace std;
 class ComboLock
 {
 private:
-    int num1;
-    int num2;
-    int num3;
+    int com1, com2, com3;
+    int num1, num2, num3;
 
 public:
-    ComboLock(int n1, int n2, int n3)
-    {
-        num1 = n1;
-        num2 = n2;
-        num3 = n3;
-    }
+    ComboLock() : com1(0), com2(0), com3(0) {}
     void reset()
     {
         num1 = 0;
@@ -23,13 +17,22 @@ public:
     }
     void turnLeft(int ticks)
     {
+        if (num1 == 0)
+        {
+            num1 = ticks;
+        }
+        else
+        {
+            num3 = ticks;
+        }
     }
     void turnRight(int ticks)
     {
+        num2 = ticks;
     }
     bool open()
     {
-        if ((num1 = 0) && (num2 = 1) && (num3 = 2))
+        if ((num1 == com1) && (num2 == com2) && (num3 == com3))
         {
             return true;
         }
@@ -42,5 +45,10 @@ public:
 
 int main()
 {
+    ComboLock cl;
+    cl.turnLeft(12);
+    cl.turnRight(23);
+    cl.turnLeft(24);
+    cout << cl.open();
     return 0;
 }
